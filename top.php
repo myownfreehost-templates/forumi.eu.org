10c5a4252fc2a70a90ee72068c1b4dfdcbc975fa
myownfreehost-templates <137825550+myownfreehost-templates@users.noreply.github.com>
2024-09-19 09:49:59 +0400

    Add files via upload

---
+++ top.php
@@ -0,0 +1,4 @@
+<!-- BOOM.GE COUNTER CODE START -->
+<script type=text/javascript src="http://links.boom.ge/jc.php?id=61271"></script>
+<noscript><a href="http://top.boom.ge/index.php?id=61271" target="_blank"><img src="http://links.boom.ge/nojs.php?id=61271" border="0" alt="BOOM.GE"></a></noscript>
+<!-- BOOM.GE COUNTER CODE END -->        
